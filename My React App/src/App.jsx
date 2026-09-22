5a4b12cd88c420e78c7b7b25557b9c2183d3641b
Ishita <ishita.iit.sharma@gmail.com>
2026-09-22 12:16:24 +0530

    Q4

--- My React App/src/App.jsx
+++ My React App/src/App.jsx
@@ -1,58 +1,12 @@
-import React, { useState } from "react";
+import React from 'react'
+import Q4 from './Components/Q4/Q4'
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch("http://localhost:4000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-      }),
-    });
-
-    const data = await response.json();
-
-    setMessage(data.message);
-  };
-
   return (
     <div>
-      <h1>User Form</h1>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <br /><br />
-
-        <input
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <br /><br />
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <h3>{message}</h3>
+      <Q4/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ My React App/src/Components/Q4/Q4.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const Q4 = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:4000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    });
+
+    const data = await response.json();
+
+    setMessage(data.message);
+  };
+
+  return (
+    <div>
+      <h2>User Form</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Submit</button>
+
+      </form>
+
+      <h3>{message}</h3>
+    </div>
+  )
+}
+
+export default Q4
